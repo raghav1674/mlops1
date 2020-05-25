f49443743d8024c43db6dd37b28d1344a46a8e73
raghav <raghav.81-cse-17@mietjammu.in>
2020-05-25 19:50:27 +0530

    allnew

--- salary_estimator.py
+++ salary_estimator.py
@@ -5,40 +5,21 @@ import pandas as pd
 #new
 model=LinearRegression() #creating the model
 
-dataset=pd.read_csv("/my_mlws/Salary_Data.csv") #loading csv file
+dataset=pd.read_csv("/my_dlws/all_storage/Salary_Data.csv") #loading csv file
 x=dataset["YearsExperience"]
 y=dataset["Salary"]
 
 X=x.values.reshape(-1,1) #convert to 2d numpy array
 
-X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=10)
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=20)
+
+
 
 model.fit(X_train,y_train)  #training the model
-y_pred=model.predict(X_test)
-
-print(mean_squared_error(y_test,y_pred))
 
 
+X_test=sc.transform(X_test)
 
+from sklearn.metrics import r2_score
+print(r2_score(y_test.values,y_pred))
 
-
-# # VISUALIZING
-import matplotlib.pyplot as plt
-plt.scatter(X_test,y_test,label="actual_datapoint")
-plt.plot(X_test,y_pred,label="predicted_datapoints")
-plt.grid(True,color="g",linestyle="--")
-# plt.xlim()
-# plt.ylim(39343.00,67938.00)
-plt.title("Testing_model")
-plt.xlabel("Year of Joining")
-plt.ylabel("Salary")
-
-
-plt.legend()
-plt.show()
-plt.savefig("visuals/salary-estimator1.jpg",dpi=400)
-
-# storing the model in storage
-
-import  joblib
-joblib.dump(model, "../created_models/my_salary-estimator.pkl1")
