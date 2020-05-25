ea0aebaca4526a973fd4e7d914e9d7a0547cc9b9
raghav1674 <45117311+raghav1674@users.noreply.github.com>
2020-05-25 22:15:03 +0530

    Update salary_estimator.py

--- salary_estimator.py
+++ salary_estimator.py
@@ -16,7 +16,7 @@ X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=20
 
 
 model.fit(X_train,y_train)  #training the model
-y_pred=model.predict(X_test,y_test)
+y_pred=model.predict(X_test)
 
 # X_test=sc.transform(X_test)
 
